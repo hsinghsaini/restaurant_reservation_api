bbc0cbf0bcd4e8e4e4095b42f4b661319411be14
hsaini734 <hsaini734@gmail.com>
2018-04-10 22:35:08 +0530

    changed subject

--- app/mailers/restaurant_mailer.rb
+++ app/mailers/restaurant_mailer.rb
@@ -2,7 +2,7 @@ class RestaurantMailer < ApplicationMailer
 
   def reservation_confirm_email(reservation_id)
     @reservation = Reservation.find_by(id: reservation_id)
-    mail(to: @reservation.restaurant.email, subject: "#{@reservation.id} - Reservation Details Updated")
+    mail(to: @reservation.restaurant.email, subject: "Reservation Confirmed")
   end
 
   def reservation_updated_email(reservation_id, changed_attributes)
