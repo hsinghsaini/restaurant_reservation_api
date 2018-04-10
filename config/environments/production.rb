ee85a1c145ad468d886e2e1207e8f7433ee296a1
hsaini734 <hsaini734@gmail.com>
2018-04-10 18:29:57 +0530

    changed host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,14 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: 'https://restaurant-reservation-api.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'restaurant-reservation-api.herokuapp.com',
     user_name:            ENV["SMTP_USER_NAME"],
     password:             ENV["SMTP_PASSWORD"],
     authentication:       'plain',
